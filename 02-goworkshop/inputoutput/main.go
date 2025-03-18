10b4b74adbcf687779f9c3dbc4e29366f86d0735
Robert Gunczer <rgunczer@gmail.com>
2025-03-18 05:42:33 +0100

    Update main.go

--- 02-goworkshop/inputoutput/main.go
+++ 02-goworkshop/inputoutput/main.go
@@ -6,8 +6,8 @@ func main() {
 	const maxSpeed byte = 60
 	const pi = 3.14
 	// var message string
-	// var message string = "Hello from Go"
-	var message = "Hello from Go"
+	var message string = "Hello from Go"
+	// var message = "Hello from Go"
 	var price = 3.44
 	// var price float32 = 3.44
 
